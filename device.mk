55142081794d161b614781be7fb08ffbd67f9132
Alessandro Privitera <alessandroprivitera@Host-013.homenet.telecomitalia.it>
2026-09-18 16:56:11 +0200

    fix: remove obsolete PRODUCT_STATIC_BOOT_CONTROL_HAL for TWRP 12.1

--- device.mk
+++ device.mk
@@ -21,11 +21,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     bootctrl.parrot
 
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.parrot \
-    libgptutils \
-    libz \
-    libcutils
+# PRODUCT_STATIC_BOOT_CONTROL_HAL removed - obsolete for TWRP 12.1
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
